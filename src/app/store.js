791127b396c5b079338117447b3d243da5fb8195
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-01-18 15:26:11 +0200

    Create Auth slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,9 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit'
+
+import authReducer from '../features/auth/authSlice'
 
 export default configureStore({
-    reducer: {
-        
-    }
+	reducer: {
+		auth: authReducer,
+	},
 })
