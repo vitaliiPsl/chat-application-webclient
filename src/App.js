4d6bbd17e9280858590d340243b10ecd969cbdc9
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-01-22 14:11:47 +0200

    Move  user load logic to App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import './App.css';
+import './App.css'
+
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+import { setUser } from './features/auth/authSlice'
+import { useGetAuthenticatedUserQuery } from './features/auth/authApi'
+
+import Navbar from './components/navbar/Navbar'
+import AuthPage from './pages/auth/AuthPage'
+import SignInForm from './components/auth/SignInForm'
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	const { user, token } = useSelector((state) => state.auth)
+
+	const { data, error, isLoading } = useGetAuthenticatedUserQuery(null, {
+		skip: !token,
+	})
+
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		if (data) {
+			dispatch(setUser(data))
+		}
+		if (error) {
+			handleUserError(error)
+		}
+	}, [data, error])
+
+	const handleUserError = (error) => {
+		console.log(error)
+	}
+
+	return (
+		<div className='App'>
+			<Navbar user={user} />
+
+			<AuthPage>
+				<SignInForm />
+			</AuthPage>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,38 +1,16 @@
 import './Navbar.css'
 
-import { useState, useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
 
-import { setUser, logout } from '../../features/auth/authSlice'
-import { useGetAuthenticatedUserQuery } from '../../features/auth/authApi'
+import { logout } from '../../features/auth/authSlice'
 
-const Navbar = ({ ...rest }) => {
-    const [open, setOpen] = useState(false)
+import Avatar from '../avatar/Avatar'
 
-	const { user, token } = useSelector((state) => state.auth)
-
-	const { data, error, isLoading } = useGetAuthenticatedUserQuery(null, {
-		skip: !token,
-	})
+const Navbar = ({ user, ...rest }) => {
+	const [open, setOpen] = useState(false)
 
 	const dispatch = useDispatch()
-
-	useEffect(() => {
-		console.log(token)
-		if (data) {
-			console.log('Data')
-			console.log(data)
-			dispatch(setUser(data))
-		}
-		if (error) {
-			console.log('Error')
-			handleUserError(error)
-		}
-	}, [data, error])
-
-	const handleUserError = (error) => {
-		console.log(error)
-	}
 
 	const logOut = () => {
 		dispatch(logout())
@@ -45,22 +23,31 @@ const Navbar = ({ ...rest }) => {
 			</div>
 
 			{user && (
-				<div className='profile-box' onClick={() => setOpen(open => !open)}>
-					<div className='profile-avatar'>
-						<span>{user.nickname[0].toUpperCase()}</span>
-					</div>
+				<div
+					className='profile-box'
+					onClick={() => setOpen((open) => !open)}
+				>
+					<Avatar placeholder={user.nickname} />
 
 					<div className='profile-nickname'>{user.nickname}</div>
 
-                    <div className="dropdown-icon">
-                        +
-                    </div>
+					<div className='dropdown-icon'>+</div>
 
-					<div className={`dropdown-box ${open ? 'open' : ''} profile-links-box`}>
-						<li className='dropdown-link profile-link preferences-link' onClick={logOut}>
+					<div
+						className={`dropdown-box ${
+							open ? 'open' : ''
+						} profile-links-box`}
+					>
+						<li
+							className='dropdown-link profile-link preferences-link'
+							onClick={logOut}
+						>
 							Preferences
 						</li>
-                        <li className='dropdown-link profile-link logout-link' onClick={logOut}>
+						<li
+							className='dropdown-link profile-link logout-link'
+							onClick={logOut}
+						>
 							Log out
 						</li>
 					</div>
