6a0fb5986709cd63d72c2e8cac831c7ca7a81b16
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-01-23 22:19:56 +0200

    Add chats slice to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import authReducer from '../features/auth/authSlice'
+import chatsReducer from '../features/chats/chatsSlice'
 
 import { apiSlice } from './api'
 
 export default configureStore({
 	reducer: {
 		auth: authReducer,
+        chats: chatsReducer,
         [apiSlice.reducerPath]: apiSlice.reducer
 	},
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
