45c6a93052f48427c72c2d0bc7235dd1644d388f
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-01-24 20:43:23 +0200

    Rewrite Chats component to load users chats from the server

--- src/components/chats/Chats.jsx
+++ src/components/chats/Chats.jsx
@@ -1,27 +1,72 @@
-import ChatItem from './ChatItem'
 import './Chats.css'
 
+import { useEffect } from 'react'
+
+import { useSelector, useDispatch } from 'react-redux'
+
+import { setChats } from '../../features/chats/chatsSlice'
+import { useGetChatsQuery } from '../../features/chats/chatsApi'
+
+import ChatListItem from './ChatListItem'
+import Button from '../button/Button'
+import Spinner from '../spinner/Spinner'
+
 const Chats = () => {
+	const { chats } = useSelector((state) => state.chats)
+
+	const dispatch = useDispatch()
+
+	const { data, error, isLoading } = useGetChatsQuery()
+
+	useEffect(() => {
+		if (data) {
+			dispatch(setChats(data))
+		}
+		if (error) {
+			console.log(error)
+			console.log(error?.data?.message)
+		}
+	}, [data, error])
+
+    const openChat = (id) => {
+        // TODO: redirect to chat with given id
+    }
+
+	const mapChatsToChatListItem = (chats) => {
+		return chats.map((chat, index) => (
+			<ChatListItem chat={chat} key={index} />
+		))
+	}
+
 	return (
 		<div className='chats'>
 			<div className='create-chat-box'>
-				<button className='create-chat-button'>Create chat</button>
+				<Button onClick={() => alert('Clicked')}>Create chat</Button>
 			</div>
 
-			<div className='chats-list'>
-				<ChatItem
-					chat={{ name: 'first chat', lastMessage: 'First message' }}
-				/>
-				<ChatItem
-					chat={{
-						name: 'Second chat',
-						lastMessage: 'Second message',
-					}}
-				/>
-				<ChatItem
-					chat={{ name: 'Third chat', lastMessage: 'Third message' }}
-				/>
+			<div className='chats-box'>
+				<label className='Chats-label'>Your chats</label>
 
+				{isLoading && (
+					<div className='center-box'>
+						<Spinner color='#888' />
+					</div>
+				)}
+
+				{!isLoading && chats && chats.length === 0 && (
+					<div className='center-box no-chats-box'>
+						<span className='no-chats-message'>
+							You don't have any chats. <br /> Click{' '}
+							<b>'Create chat'</b> to start chatting
+						</span>
+					</div>
+				)}
+
+				{!isLoading && chats && chats.length > 0 && (
+					<div className='chats-list'>
+						{mapChatsToChatListItem(chats)}
+					</div>
+				)}
 			</div>
 		</div>
 	)
