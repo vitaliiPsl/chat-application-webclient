1c96b55c2f4be41b7fcbb78a244247563e2d2307
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-01-25 21:28:24 +0200

    Add messages property to chatsSlice

--- src/features/chats/chatsSlice.js
+++ src/features/chats/chatsSlice.js
@@ -1,7 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-    chat: null,
+	chat: null,
+	messages: null,
 	chats: [],
 }
 
@@ -9,10 +10,14 @@ export const chatsSlice = createSlice({
 	name: 'chats',
 	initialState,
 	reducers: {
-        setChat: (state, action) => {
-            let chat = action.payload
-            state.chat = chat
-        },
+		setChat: (state, action) => {
+			let chat = action.payload
+			state.chat = chat
+		},
+		setMessages: (state, action) => {
+			let messages = action.payload
+			state.messages = messages
+		},
 		setChats: (state, action) => {
 			let chats = action.payload
 			state.chats = chats
@@ -20,5 +25,5 @@ export const chatsSlice = createSlice({
 	},
 })
 
-export const { setChat, setChats } = chatsSlice.actions
+export const { setChat, setMessages, setChats } = chatsSlice.actions
 export default chatsSlice.reducer
