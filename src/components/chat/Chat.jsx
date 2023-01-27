4bdf7b7b554faee0742ea34b30dc4a5b92bfd86c
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-01-27 19:38:20 +0200

    Implement loading logic

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,29 +1,100 @@
 import './Chat.css'
 
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
+
+import { useSelector, useDispatch } from 'react-redux'
+import { setChat, setMessages } from '../../features/chats/chatsSlice'
+
+import {
+	useGetChatQuery,
+	useGetChatMessagesQuery,
+	useSendMessageMutation,
+} from '../../features/chats/chatsApi'
 
 import Avatar from '../avatar/Avatar'
 import ChatMessage from './ ChatMessage'
 import TextField from '../text-field/TextField'
 import Button from '../button/Button'
+import Spinner from '../spinner/Spinner'
+import ChatMessageGroup from './ChatMessageGroup'
+
+const chatId = 'bb21ec87-18dc-4af0-be5a-9e84641693d0'
 
 const Chat = () => {
-	const [chat, setChat] = useState({
-		name: 'First chat',
+	const { user } = useSelector((state) => state.auth)
+	const { chat, messages } = useSelector((state) => state.chats)
+
+	const messageInputRef = useRef()
+
+	const dispatch = useDispatch()
+
+	const {
+		data: chatData,
+		error: chatError,
+		isLoading: chatIsLoading,
+	} = useGetChatQuery(chatId)
+
+	const {
+		data: messagesData,
+		error: messagesError,
+		isLoading: messagesIsLoading,
+	} = useGetChatMessagesQuery(chatId, {
+		skip: !chat,
 	})
 
-	const [messages, setMessages] = useState([
-		{ content: 'Hello, m8', sentAt: '6.45' },
-		{ content: 'Hi. How are you?', sentAt: '6.47' },
+	const [
+		sendMessage,
 		{
-			content:
-				'Fine. Working on a new project. Trying to build chat application',
-			sentAt: '6.50',
+			data: sendMessageData,
+			error: sendMessageError,
+			sendMessageIsLoading,
 		},
-		{ content: 'That is cool', sentAt: '6.55' },
-	])
+	] = useSendMessageMutation()
 
-	return (
+	useEffect(() => {
+		if (chatData) {
+			dispatch(setChat(chatData))
+		}
+		if (chatError) {
+			console.log(chatError?.data?.message)
+		}
+	}, [chatData, chatError])
+
+	useEffect(() => {
+		if (messagesData) {
+			dispatch(setMessages(messagesData.content))
+		}
+		if (messagesError) {
+			console.log(chatError?.data?.message)
+		}
+	}, [messagesData, messagesError])
+
+	useEffect(() => {
+		if (sendMessageData) {
+			console.log(sendMessageData)
+		}
+		if (sendMessageError) {
+			console.log(sendMessageError)
+		}
+	}, [sendMessageData, sendMessageError])
+
+	const handleSendMessage = (e) => {
+		e.preventDefault()
+
+		let content = messageInputRef.current.value
+
+		if (content && content.trim()) {
+			let message = { content }
+			console.log(message)
+			sendMessage({ id: chatId, message })
+		}
+
+		messageInputRef.current.value = ''
+	}
+
+	return !chat ? (
+		<Spinner />
+	) : (
 		<div className='chat'>
 			<div className='chat-info-bar'>
 				<Avatar placeholder={chat.name} />
@@ -31,21 +102,33 @@ const Chat = () => {
 				<div className='chat-options'></div>
 			</div>
 
-			<div className='chat-message-box'>
-				{messages.map((message, index) => (
-					<ChatMessage message={message} key={index} />
-				))}
-                <ChatMessage message={messages[0]} right={true} key={123} />
+			<div className='chat-messages-box'>
+				
 			</div>
 
 			<div className='chat-message-input-box'>
-				<TextField
-					className='chat-message-input'
-					name={'message'}
-					placeholder={'Message...'}
-				/>
+				<form
+					className='chat-message-form'
+					onSubmit={handleSendMessage}
+				>
+					<TextField
+						className='chat-message-input'
+						name={'message'}
+						placeholder={'Message...'}
+						reference={messageInputRef}
+					/>
 
-				<Button className='chat-message-input-button'>Send</Button>
+					<Button
+						type={'submit'}
+						className='chat-message-input-button'
+					>
+						{sendMessageIsLoading ? (
+							<Spinner size={19} color={'#888'} />
+						) : (
+							'Send'
+						)}
+					</Button>
+				</form>
 			</div>
 		</div>
 	)
