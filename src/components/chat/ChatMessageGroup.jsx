bca60b36ae4a62043ef520915bd1df209683ed4d
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-01-27 19:36:55 +0200

    Create ChatMessageGroup component that represents group of messages send by user in given moment of conversation

---
+++ src/components/chat/ChatMessageGroup.jsx
@@ -0,0 +1,37 @@
+import './ChatMessageGroup.css'
+
+import Avatar from '../avatar/Avatar'
+import ChatMessage from './ ChatMessage'
+
+const ChatMessageGroup = ({ group, user }) => {
+	let author = group[0]
+	let messages = group[1]
+
+	let isAuthor = user.id == author.id
+
+	const mapMessages = () => {
+		return messages?.map((message, index) => (
+			<ChatMessage message={message} user={user} key={index} />
+		))
+	}
+
+	return (
+		<div className={`chat-message-group-box ${isAuthor ? 'author' : ''}`}>
+			{!isAuthor && <Avatar placeholder={author.nickname} />}
+
+			<div className='chat-messages-group-messages-box'>
+				{!isAuthor && (
+					<span className='chat-messages-group-author-nickname'>
+						{author.nickname}
+					</span>
+				)}
+
+				<div className='chat-messages-group-messages-list'>
+					{mapMessages()}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default ChatMessageGroup
