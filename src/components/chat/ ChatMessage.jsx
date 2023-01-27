6a2fffc50d1b77b5e8c3052f6c6118fffec7ecaf
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-01-27 19:36:15 +0200

    Update ChatMessage to display timestamp

--- src/components/chat/ ChatMessage.jsx
+++ src/components/chat/ ChatMessage.jsx
@@ -1,10 +1,15 @@
-import './ChatMessage.css'
+import './ChatMessageGroup.css'
 
-const ChatMessage = ({ message, right }) => {
+const ChatMessage = ({ message, user }) => {
+	let right = message.user.id === user.id
+
+	let timeOptions = { hour12: true, hour: '2-digit', minute: '2-digit'}
+	let time = new Date(message.sentAt).toLocaleTimeString('en', timeOptions)
+
 	return (
-		<div className={`chat-message ${right ? 'right' : ''}`}>
+		<div className={`chat-message`}>
 			<div className='content'>{message.content}</div>
-			<div className='timestamp'>{message.sentAt}</div>
+			<div className='timestamp'>{time}</div>
 		</div>
 	)
 }
