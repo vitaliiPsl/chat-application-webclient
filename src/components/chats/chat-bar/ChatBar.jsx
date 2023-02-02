d28295654b60a60f5d911afc7d953e7b20f3a2aa
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-02-02 11:30:53 +0200

    Extracted dropdown from ChatBar into a prop

--- src/components/chats/chat-bar/ChatBar.jsx
+++ src/components/chats/chat-bar/ChatBar.jsx
@@ -1,39 +1,6 @@
 import './ChatBar.css'
 
-import Dropdown from '../../dropdown/Dropdown'
-import MaterialIcon from '../../material-icon/MaterialIcon'
-
-import { useSelector } from 'react-redux'
-import { useRemoveChatMemberMutation } from '../../../features/chats/chatsApi'
-import { useNavigate } from 'react-router-dom'
-
-const ChatBar = ({ icon, chat, onClick }) => {
-	const { user } = useSelector((state) => state.auth)
-
-	const navigate = useNavigate()
-
-	const [removeChatMember, { isLoading }] =
-		useRemoveChatMemberMutation()
-
-	const leaveChat = async () => {
-		let payload = { chatId: chat.id, userId: user.id }
-
-		try {
-			await removeChatMember(payload).unwrap()
-			navigate('/chats')
-		} catch (err) {
-			console.log(err)
-			console.log(err?.data?.message)
-		}
-	}
-
-	const getChatOptions = () => {
-		let options = new Map()
-		options.set('leave', () => leaveChat())
-
-		return options
-	}
-
+const ChatBar = ({ icon, chat, dropdown, onClick }) => {
 	return (
 		<div className='chat-bar'>
 			<div className='chat-bar-icon'>{icon}</div>
@@ -42,13 +9,7 @@ const ChatBar = ({ icon, chat, onClick }) => {
 				{chat.name}
 			</div>
 
-			<div className='chat-bar-options'>
-				<Dropdown options={getChatOptions()}>
-					<div className='chat-options-icon-box'>
-						<MaterialIcon icon={'more_vert'} />
-					</div>
-				</Dropdown>
-			</div>
+			<div className='chat-bar-dropdown'>{dropdown}</div>
 		</div>
 	)
 }
