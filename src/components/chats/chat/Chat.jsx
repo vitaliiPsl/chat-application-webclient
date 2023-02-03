6221936311f91bf735f627850de417e5e158f5f5
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-02-03 21:31:17 +0200

    Add websocket subscription to chat messages

--- src/components/chats/chat/Chat.jsx
+++ src/components/chats/chat/Chat.jsx
@@ -1,11 +1,12 @@
 import './Chat.css'
 
-import { useEffect, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 import { useSelector, useDispatch } from 'react-redux'
 import {
 	setChat,
 	setMessages,
+	addMessage,
 } from '../../../features/chats/chatsSlice'
 
 import {
@@ -16,6 +17,8 @@ import {
 
 import { useParams, useNavigate } from 'react-router-dom'
 
+import { Client } from '@stomp/stompjs'
+
 import Avatar from '../../avatar/Avatar'
 import TextField from '../../text-field/TextField'
 import Button from '../../button/Button'
@@ -24,11 +27,16 @@ import Spinner from '../../spinner/Spinner'
 import ChatMessageGroup from './ChatMessageGroup'
 import ChatBar from '../chat-bar/ChatBar'
 
+const STOMP_BROKER_URL = 'ws://localhost:8080/ws'
+
 const Chat = () => {
 	const { chatId } = useParams()
 
-	const { user } = useSelector((state) => state.auth)
+	const { user, token } = useSelector((state) => state.auth)
 	const { chat, messages } = useSelector((state) => state.chats)
+
+	const [stompClient, setStompClient] = useState()
+	const [stompSubscription, setStompSubscription] = useState()
 
 	const messageInputRef = useRef()
 
@@ -59,17 +67,17 @@ const Chat = () => {
 
 	useEffect(() => {
 		if (chatData) {
-            // set chat
+			// set chat
 			dispatch(setChat(chatData))
-            
-            // fetch messages
+
+			// fetch messages
 			getChatMessagesQuery(chatId, false)
 		}
 		if (chatError) {
 			console.log(chatError?.data?.message)
 		}
 		if (messagesData) {
-            // set messages
+			// set messages
 			dispatch(setMessages(messagesData.content))
 		}
 		if (messagesError) {
@@ -77,8 +85,55 @@ const Chat = () => {
 		}
 	}, [chatData, chatError, messagesData, messagesError])
 
+	useEffect(() => {
+		if (chat) {
+			openStompConnection(chatId)
+
+			return () => {
+				if (stompClient) {
+					stompClient.deactivate()
+				}
+			}
+		}
+	}, [chat])
+
 	const openChatDetails = () => {
 		navigate(`/chats/${chatId}/details`)
+	}
+
+	const openStompConnection = (chatId) => {
+		const client = new Client({
+			brokerURL: STOMP_BROKER_URL,
+			connectHeaders: { Authorization: `Bearer ${token}` },
+
+			debug: function (str) {
+				console.log(str)
+			},
+		})
+
+		client.onConnect = function (frame) {
+			const subscription = client.subscribe(
+				`/topic/chats/${chatId}/messages`,
+				onMessage,
+				{ Authorization: `Bearer ${token}` }
+			)
+
+			setStompClient((stompClient) => client)
+			setStompSubscription((stompSubscription) => subscription)
+		}
+
+		client.onStompError = function (frame) {
+			console.log('Broker reported error: ' + frame.headers['message'])
+			console.log('Additional details: ' + frame.body)
+		}
+
+		client.activate()
+	}
+
+	const onMessage = (messageData) => {
+		let message = JSON.parse(messageData.body)
+
+		dispatch(addMessage(message))
 	}
 
 	const handleSendMessage = (e) => {
