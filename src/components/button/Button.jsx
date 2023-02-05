ee7201b414d69087cb9848c373591ba7bd6f1eb1
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-02-05 18:43:11 +0200

    Replace Button styles with tailwind

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,8 +1,10 @@
-import './Button.css'
-
 const Button = ({ className = '', children, type, onClick }) => {
 	return (
-		<button className={`btn ${className}`} type={type} onClick={onClick}>
+		<button
+			className={`btn ${className} min-w-40 w-full px-3 py-2 flex items-center justify-center text-base bg-white text-zinc-800 border border-zinc-800 rounded-md cursor-pointer duration-500 hover:text-white hover:bg-zinc-800`}
+			type={type}
+			onClick={onClick}
+		>
 			{children}
 		</button>
 	)
