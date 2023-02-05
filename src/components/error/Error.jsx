e3e3b000f915685355710d1c8cac8803ee60e0b0
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-02-05 18:58:40 +0200

    Replace Error styles with tailwind

--- src/components/error/Error.jsx
+++ src/components/error/Error.jsx
@@ -1,15 +1,13 @@
-import './Error.css'
-
 import MaterialIcon from '../material-icon/MaterialIcon'
 
 const Error = ({ message, onClose }) => {
 	return (
-		<div className='error'>
-			<div className='error-message'>
+		<div className='error py-3 px-6 flex items-center justify-between border-1 border-red-700 rounded-lg'>
+			<div className='error-message flex-1 text-center text-red-700'>
 				<p>{message}</p>
 			</div>
 
-			<div className='error-close-icon'>
+			<div className='error-close-icon w-8 h-8'>
 				<MaterialIcon icon={'close'} onClick={onClose} />
 			</div>
 		</div>
