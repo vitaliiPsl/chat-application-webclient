fc78f1b78d9e69561c606d2cb3d593c7f3db8937
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-02-06 13:48:25 +0200

    Set column as default direction

--- src/components/layout/OuterBox.jsx
+++ src/components/layout/OuterBox.jsx
@@ -1,4 +1,4 @@
-const OuterBox = ({ children, className, direction = 'row' }) => {
+const OuterBox = ({ children, className, direction = 'col' }) => {
 	return (
 		<div
 			className={`outer-box ${className} p-4 flex flex-${direction} gap-4 bg-zinc-200 rounded-lg`}
