7fa5309b30465451b7f1fd4100d555de54795ec7
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-02-07 18:34:34 +0200

    Replace ChatMessage and ChatMessageGroup styles with tailwind

--- src/components/chats/chat/chat-messages/ChatMessage.jsx
+++ src/components/chats/chat/chat-messages/ChatMessage.jsx
@@ -1,14 +1,25 @@
-import './ChatMessageGroup.css'
+import { useSelector } from 'react-redux'
 
 const ChatMessage = ({ message }) => {
+	const { user } = useSelector((state) => state.auth)
 
-	let timeOptions = { hour12: true, hour: '2-digit', minute: '2-digit'}
+	let timeOptions = { hour12: true, hour: '2-digit', minute: '2-digit' }
 	let time = new Date(message.sentAt).toLocaleTimeString('en', timeOptions)
 
+	let isAuthor = message.user.id === user.id
+
 	return (
-		<div className={`chat-message`}>
+		<div
+			className={`chat-message min-w-20 w-fit px-2 py-1 flex flex-col text-white bg-zinc-800 border border-zinc-800 rounded-lg`}
+		>
 			<div className='content'>{message.content}</div>
-			<div className='timestamp'>{time}</div>
+			<div
+				className={`timestamp text-xs ${
+					isAuthor ? 'self-start' : 'self-end'
+				}`}
+			>
+				{time}
+			</div>
 		</div>
 	)
 }
--- src/components/chats/chat/chat-messages/ChatMessageGroup.jsx
+++ src/components/chats/chat/chat-messages/ChatMessageGroup.jsx
@@ -1,22 +1,28 @@
-import './ChatMessageGroup.css'
-
 import Avatar from '../../../avatar/Avatar'
 
 const ChatMessageGroup = ({ messages, author, user }) => {
 	let isAuthor = user.id === author.id
 
 	return (
-		<div className={`chat-message-group-box ${isAuthor ? 'author' : ''}`}>
-			{!isAuthor && <Avatar placeholder={author.nickname} />}
+		<div
+			className={`chat-message-group-box ${
+				isAuthor ? 'author self-end' : ''
+			} flex gap-2`}
+		>
+			{!isAuthor && (
+				<div className='chat-message-group-avatar-box self-end'>
+					<Avatar placeholder={author.nickname} />
+				</div>
+			)}
 
-			<div className='chat-messages-group-messages-box'>
+			<div className='chat-messages-group-messages-box flex flex-col items-start'>
 				{!isAuthor && (
 					<span className='chat-messages-group-author-nickname'>
 						{author.nickname}
 					</span>
 				)}
 
-				<div className='chat-messages-group-messages-list'>
+				<div className={`chat-messages-group-messages-list flex flex-col-reverse gap-2 ${isAuthor && 'items-end'}`}>
 					{messages}
 				</div>
 			</div>
